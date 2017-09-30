31a1d015e6a1190253e0d3e25d45b5b883f94bdc
dyuk <dyukit99@gmail.com>
2017-09-30 14:50:46 +0300

    new

---
+++ build/js/error.js
@@ -0,0 +1,11 @@
+'use strict'
+$(document).ready(function() {
+new SiriWave({
+  height: 100,
+  speed: 0.05,
+  container: document.getElementById('error'),
+  autostart: true,
+  cover:true,
+  color:'#956280',
+});
+});
